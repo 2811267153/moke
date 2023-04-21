00ddb54d414350760283c1746674955057874a4e
xuzesen <2811267153@qq.com>
2023-04-21 17:23:18 +0800

    优化歌词滚动进度算法, 歌词滚动更准确

--- src/components/lyric/Lryic.tsx
+++ src/components/lyric/Lryic.tsx
@@ -10,24 +10,37 @@ type LyricItem = [number, string];
 export function Lyrics({ lyrics, currentTime }: LyricsProps) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [marginTop, setMarginTop] = useState(20);
+  const [lineHeight, setLineHeight] = useState(0);
   const lyricRef = useRef<HTMLDivElement>(null)
+  const ContainerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if(ContainerRef.current) {
+      const { height } = ContainerRef.current.getBoundingClientRect();
+      const { length } = lyrics;
+      console.log(height, length);
+      const lineHeight = height / length;
+      setLineHeight(lineHeight);
+    }
+  }, [lyrics, ]);
+
   useEffect(() => {
     const nextIndex = lyrics.findIndex(([time]) => (time / 1000) > currentTime)
     if (nextIndex !== currentIndex && nextIndex !== -1) {
-      setCurrentIndex( nextIndex -1);
+      setCurrentIndex(nextIndex - 1);
     }
   }, [currentTime]);
+  useEffect(() => {
+    setMarginTop(-(currentIndex * lineHeight));
+  }, [currentIndex, lineHeight]);
   const handleItemClick = (time: string | number) => {
     PubSub.publish("UpDataTime", time)
   }
-  useEffect(() => {
-    setMarginTop(-(currentIndex * 25));
-  }, [currentIndex]);
 
   return (
     <div className={styles["lyrics"]}>
       {
-        lyrics.length != 0 ? (<div style={{ marginTop }} className={styles.lyrics_container}>
+        lyrics.length != 0 ? (<div ref={ContainerRef}  style={{ marginTop }} className={styles.lyrics_container}>
           {lyrics?.map((line, i) => (
             <div
               key={i}
